882541e75f16f9f3d56180441fd86b0570f76e48
faroutroy <68562263+faroutroy@users.noreply.github.com>
2022-02-05 12:14:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SearchPets from './components/SearchPets';
+
+function App() {
+  return (
+    <div className='App'>
+      <SearchPets />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchPets.jsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react';
+import { Select, Input, Card, Col, Row } from 'antd';
+import Moment from 'react-moment';
+
+const { Search } = Input;
+const { Option } = Select;
+
+const SearchPets = () => {
+  const [searchterm, setSearchterm] = useState('');
+  const [pets, setPets] = useState([]);
+  const [filterAge, setFilterAge] = useState('');
+  const [filteredRes, setFilteredRes] = useState([]);
+
+  useEffect(() => {
+    if (filterAge === 'Filter') fetchPetsByAge();
+    else fetchPets();
+  }, [filterAge]);
+
+  const filterPets = (searchval) => {
+    setSearchterm(searchval);
+    if (searchterm != '') {
+      const filteredPets = pets?.filter((pet) =>
+        pet.name.toLowerCase().includes(searchterm.toLowerCase())
+      );
+      setFilteredRes(filteredPets);
+    } else {
+      setFilteredRes(pets);
+    }
+  };
+
+  const fetchPets = () => {
+    fetch(
+      'https://60d075407de0b20017108b89.mockapi.io/api/v1/animals?sortBy=createdAt&order=desc'
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPets(data);
+      });
+  };
+
+  const fetchPetsByAge = () => {
+    fetch(
+      'https://60d075407de0b20017108b89.mockapi.io/api/v1/animals?sortBy=bornAt&order=desc'
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPets(data);
+      });
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="center">My Pets</h1>
+        <div className="search-pets">
+          <Search
+            placeholder="Search pets"
+            size="large"
+            onChange={(e) => filterPets(e.target.value)}
+          />
+          <Select
+            showSearch
+            className="select-pets"
+            placeholder="Filter"
+            onChange={(value) => setFilterAge(value)}
+          >
+            <Option value="">Filter</Option>
+            <Option value="Filter">By Age</Option>
+          </Select>
+        </div>
+
+        <Row gutter={[32, 32]} className="pet-card-container">
+          {searchterm.length > 1
+            ? filteredRes?.map((pet) => (
+                <Col xs={24} sm={12} lg={6} className="pet-card" key={pet.id}>
+                  <Card
+                    title={`${pet.name}`}
+                    extra={
+                      <img
+                        src={`${pet.avatar}`}
+                        className="pet-image"
+                        alt="img"
+                      />
+                    }
+                  >
+                    <h4>
+                      Age:{' '}
+                      <Moment fromNow ago>
+                        {pet.bornAt}
+                      </Moment>{' '}
+                      old
+                    </h4>
+                  </Card>
+                </Col>
+              ))
+            : pets?.map((pet) => (
+                <Col xs={24} sm={12} lg={6} className="pet-card" key={pet.id}>
+                  <Card
+                    title={`${pet.name}`}
+                    extra={
+                      <img
+                        src={`${pet.avatar}`}
+                        className="pet-image"
+                        alt="img"
+                      />
+                    }
+                  >
+                    <h4>
+                      Age:{' '}
+                      <Moment fromNow ago>
+                        {pet.bornAt}
+                      </Moment>{' '}
+                      old
+                    </h4>
+                  </Card>
+                </Col>
+              ))}
+        </Row>
+      </div>
+    </>
+  );
+};
+
+export default SearchPets;
